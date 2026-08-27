87c5013f69c5c679b702b9c8088837d1bc5ba099
Ayush chouksey <achouksey516@gmail.com>
2026-08-28 00:36:57 +0530

    Add solution for Largest Odd Number in String

---
+++ solutions/unknown/Largest Odd Number in String.cpp
@@ -0,0 +1,18 @@
+// Title: Largest Odd Number in String
+            // Difficulty: Unknown
+            // Language: C++
+            // Link: https://leetcode.com/problems/largest-odd-number-in-string/
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+
+        for (int i = n - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2 == 1) {
+                return num.substr(0, i + 1);
+            }
+        }
+
+        return "";
+    }
