47145f3edf15ef9e9831fd4668d2972d0e73cf70
Ayush chouksey <achouksey516@gmail.com>
2026-09-06 00:46:34 +0530

    Add solution for Largest Odd Number in String

--- solutions/easy/Largest Odd Number in String.cpp
+++ solutions/easy/Largest Odd Number in String.cpp
@@ -6,9 +6,9 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        int n = num.size();
+        //int n = num.size();
 
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = num.size() - 1; i >= 0; i--) {
             if ((num[i] - '0') % 2 == 1) {
                 return num.substr(0, i + 1);
             }
@@ -16,3 +16,4 @@ public:
 
         return "";
     }
+};
